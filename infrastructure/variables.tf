de2ad049fe5ccb161167daca6ab895793c06e259
Semenu Aggor <semenu.aggor@HMCTS.NET>
2022-07-14 15:21:55 +0100

    HMAN-323 Remove unrecognised tokens for now

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,6 +1,10 @@
-variable "product" {}
+variable "product" {
+  default = "ccd"
+}
 
-variable "component" {}
+variable "component" {
+  default = "next-hearing-date-updater"
+}
 
 variable "location" {
   default = "UK South"
@@ -13,5 +17,5 @@ variable "subscription" {}
 variable "deployment_namespace" {}
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
