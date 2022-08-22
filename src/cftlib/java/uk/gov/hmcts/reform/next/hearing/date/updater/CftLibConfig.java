dc22665c35c17361389ad334ac6cca5490cb2c57
Semenu Aggor <semenu.aggor@HMCTS.NET>
2022-08-22 16:46:37 +0100

    HMAN-381 Investigate load creds with CFTLibConfig

--- src/cftlib/java/uk/gov/hmcts/reform/next/hearing/date/updater/CftLibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/next/hearing/date/updater/CftLibConfig.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.next.hearing.date.updater;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
 import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
 
 @Component
 public class CftLibConfig implements CFTLibConfigurer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CftLibConfig.class);
 
     @Override
     public void configure(CFTLib lib) throws Exception {
@@ -14,17 +18,25 @@ public class CftLibConfig implements CFTLibConfigurer {
     }
 
     private void createCcdRoles(CFTLib lib) {
+        LOGGER.info("About to create roles......................");
+        System.out.println("About to create roles......................");
         lib.createRoles(
             "caseworker",
             "caseworker-autotest1",
+            "caseworker-befta_master",
             "ccd-import",
             "next-hearing-date-admin"
         );
+        System.out.println("Finished creating roles......................");
+        LOGGER.info("Finished creating roles......................");
     }
 
     private void createIdamUsers(CFTLib lib) {
+        LOGGER.info("About to create Idam users......................");
         lib.createIdamUser("ccd.docker.default@hmcts.net", "ccd-import");
         lib.createIdamUser("auto.test.cnp@gmail.com", "caseworker", "caseworker-autotest1", "ccd-import");
         lib.createIdamUser("next.hearing.date.admin@gmail.com", "next-hearing-date-admin");
+        lib.createIdamUser("master.caseworker@gmail.com", "caseworker", "caseworker-befta_master");
+        LOGGER.info("Finished creating Idam users......................");
     }
 }
