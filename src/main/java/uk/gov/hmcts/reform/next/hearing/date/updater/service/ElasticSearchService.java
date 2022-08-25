0037b67a74c1b09dbad5ee9608a4c49364283b0e
Semenu Aggor <semenu.aggor@HMCTS.NET>
2022-08-25 17:08:55 +0100

    HMAN-381 Remove unused imports

--- src/main/java/uk/gov/hmcts/reform/next/hearing/date/updater/service/ElasticSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/next/hearing/date/updater/service/ElasticSearchService.java
@@ -3,11 +3,9 @@ package uk.gov.hmcts.reform.next.hearing.date.updater.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import uk.gov.hmcts.reform.next.hearing.date.updater.repository.ElasticSearchRepository;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * HMAN-326 - Return a paginated result set off cases for all caseTypes that needs NextHearing Date to be set.
