dd34d431b05ebc1bc4029098679c5b21b1b2b004
Matt Nayler <matt.nayler@cgi.com>
2022-09-28 13:12:08 +0100

    Re-apply sleep in ES test config.
       [HMAN-342](https://tools.hmcts.net/jira/browse/HMAN-342)
    
    Re-apply sleep call to ES test config to avoid issues in pipeline.

--- src/integrationTest/java/uk/gov/hmcts/reform/next/hearing/date/updater/config/es/ElasticSearchIndexCreator.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/next/hearing/date/updater/config/es/ElasticSearchIndexCreator.java
@@ -15,6 +15,7 @@ import uk.gov.hmcts.reform.next.hearing.date.updater.utils.ElasticSearchIntegrat
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.client.RequestOptions.DEFAULT;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -32,8 +33,10 @@ public class ElasticSearchIndexCreator {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @SuppressWarnings("java:S2925")
     public void insertDataIntoElasticsearch(final String indexName,
-                                            final List<CaseData> caseDataEntities) throws IOException {
+                                            final List<CaseData> caseDataEntities
+    ) throws IOException, InterruptedException {
 
         final String caseIndex = elasticSearchIntegrationTestUtils.getIndexName(indexName);
 
@@ -42,6 +45,9 @@ public class ElasticSearchIndexCreator {
         final BulkResponse bulkResponse = elasticsearchClient.bulk(bulkRequest, DEFAULT);
 
         assertFalse(bulkResponse.hasFailures());
+
+        // java:S2925 :: use sleep to workaround issue with ES IT setup in pipeline
+        TimeUnit.MINUTES.sleep(1);
 
         refreshIndex(caseIndex);
     }
