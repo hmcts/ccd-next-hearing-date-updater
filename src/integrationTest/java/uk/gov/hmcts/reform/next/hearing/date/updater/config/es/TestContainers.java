c2186acdba006fff34fc84f1bdfffa0eea5cea27
Matt Nayler <matt.nayler@cgi.com>
2022-09-29 09:14:48 +0100

    HMAN-342 ES version used in integration tests
       [HMAN-342](https://tools.hmcts.net/jira/browse/HMAN-342)
    
    Adjust ES version used in integration tests.

--- src/integrationTest/java/uk/gov/hmcts/reform/next/hearing/date/updater/config/es/TestContainers.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/next/hearing/date/updater/config/es/TestContainers.java
@@ -13,18 +13,46 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Testcontainers
 @ContextConfiguration(initializers = {TestContainers.ElasticsearchInitializer.class})
 public class TestContainers {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestContainers.class);
-    private static final String IMAGE = "docker.elastic.co/elasticsearch/elasticsearch:7.17.3";
-    private static final ElasticsearchContainer ELASTICSEARCH_CONTAINER = new ElasticsearchContainer(IMAGE)
+    private static final String IMAGE = "docker.elastic.co/elasticsearch/elasticsearch:%s";
+    private static final ElasticsearchContainer ELASTICSEARCH_CONTAINER = new ElasticsearchContainer(getImageName())
         .withLogConsumer(new Slf4jLogConsumer(LOGGER))
         .waitingFor(Wait.forListeningPort());
 
     static {
         if (!ELASTICSEARCH_CONTAINER.isRunning()) {
             ELASTICSEARCH_CONTAINER.start();
+        }
+    }
+
+    @SuppressWarnings("RegExpUnnecessaryNonCapturingGroup")
+    private static String getImageName() {
+        // regex to search only JAR paths ...
+        // ... and extract the jar version (see output of first match group)
+        final String regex = "^(?:jar:.*-)(.*)(?:\\.jar!.*)$";
+
+        // use ES client to find compatible image version
+        Class<?> clazz =  org.elasticsearch.client.RestHighLevelClient.class;
+
+        // load class path including jar information when packaged
+        final String className = clazz.getSimpleName() + ".class";
+        final String classPath = Objects.requireNonNull(clazz.getResource(className)).toString();
+
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(classPath);
+
+        if (matcher.find()) {
+            return String.format(IMAGE, matcher.group(1));
+        } else {
+            // NB: this will generate a descriptive ExceptionInInitializerError if version number not found
+            return "Error: ES image version number not found";
         }
     }
 
